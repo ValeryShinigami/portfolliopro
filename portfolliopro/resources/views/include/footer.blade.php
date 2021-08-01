c96647dcc00426cd3367c51392caa6b275d13767
valery kalombo <valerykalombo@hotmail.fr>
2021-08-01 18:44:07 +0200

    add footer

---
+++ portfolliopro/resources/views/include/footer.blade.php
@@ -0,0 +1,59 @@
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="skill row">
+				<div class="competence col-md-6 d-flex flex-column align-items-center justify-content-center">
+					<h3 class="comp mt-2 mb-3" id="comp1">
+						Compétences
+					</h3>
+					<ul>
+						<li class="list-item mb-2">
+							JAVASCRIPT
+						</li>
+						<li class="list-item mb-2">
+							JQUERY
+						</li>
+						<li class="list-item mb-2">
+							BOOTSTRAP
+						</li>
+						<li class="list-item mb-2">
+							GIT/GITHUB
+						</li>
+						<li class="list-item mb-2">
+							WORDPRESS
+						</li>
+						<li class="list-item mb-2">
+							PHP
+						</li>
+						<li class="list-item mb-2">
+							LARAVEL
+						</li>
+						<li class="list-item mb-2">
+							SQL / MYSQL / MERISE
+						</li>
+						<li class="list-item mb-2">
+							HTML5 
+						</li>
+						<li class="list-item mb-2">
+							CSS3
+							
+						</li>
+						<li class="list-item">
+							SASS
+							
+						</li>
+					</ul>
+				</div>
+				<div class="imgLogo col-md-6">
+					<img class="img-fluid" src="{{asset('image/amp.jpeg')}}" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="copyright">© 2021 Valéry Kalombo - Développeur web Paris / Ile-de-France - Mentions légales</p>
+		</div>
+	</div>
+</div>
