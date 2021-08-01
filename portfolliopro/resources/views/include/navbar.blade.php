dceed7068037a173352fed47e1d9eff69692e6a7
valery kalombo <valerykalombo@hotmail.fr>
2021-08-01 18:03:00 +0200

    add navbar

---
+++ portfolliopro/resources/views/include/navbar.blade.php
@@ -0,0 +1,41 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <!--<a class="navbar-brand" href="#">Navbar w/ text</a>-->
+      <div class="test">
+        <img src="https://img.icons8.com/material/24/ffffff/mail.png"/> <a class="lien" href="mailto:valerykalombo@hotmail.fr">valerykalombo@hotmail.fr</a>
+
+      </div>
+      <div class="test">
+             <img src="https://img.icons8.com/metro/20/ffffff/phone.png"/> <a class="lien" href="tel:0666652572">0666652572</a>
+
+      </div>
+    </div>
+</nav>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Valéry<br>KALOMBO</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="breadcrumb me-auto mb-2 mb-lg-0">
+          <li class="breadcrumb-item">
+            <a href="#">Accueil</a>
+          </li>
+          <li class="breadcrumb-item">
+            <a href="#">Projets</a>
+          </li>
+          <li class="breadcrumb-item">
+            <a href="#">Compétences</a>
+          </li>
+          <li class="breadcrumb-item">
+            <a href="#">Profil/CV</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+    
